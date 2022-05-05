cd07e057fe6b26f6a1d114764fb6ca85198b8749
Cole Rossman <colerossman99@gmail.com>
2022-05-05 12:48:19 -0700

    handlesubmit added to auth

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useState} from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 
 export default function Auth() {
+  const { login } = useUser();
+  const location = useLocation();
+  const history = useHistory();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    // add preventDefault to prevent 1996 behavior in the form
+    try{
+      event.preventDefault();
+      await login(email,password);
+      const url = location.state.origin ? location.state.origin.pathname: '/';
+      history.replace(url);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div>Auth</div>
   )
